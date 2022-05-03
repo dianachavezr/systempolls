c300c1f35565fe0a2779ef9ce67f65efd84e16ec
DIANACH <dianepatrich@gmail.com>
2022-05-03 15:14:57 -0500

    make it better

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python manage.py runserver 0.0.0.0:SPORT
+web: gunicorn systempolls.wsgi --log-file -
--- systempolls/urls.py
+++ systempolls/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include,  path
 
@@ -21,4 +23,4 @@ from django.urls import include,  path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('polls/', include(('polls.urls', 'polls'), namespace="polls.urls")),
-    ]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
